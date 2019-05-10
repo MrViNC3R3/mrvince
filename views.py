379d4471fe383cafe8911ac319243c33f353b3b1
MrViNC3R3 <rogervincere2@gmail.com>
2019-05-10 10:43:37 +0530

    Update views.py

--- views.py
+++ views.py
@@ -5,4 +5,4 @@ source = 'https://github.com/zeit/now-examples/tree/master/python-django'
 css = '<link rel="stylesheet" href="/css/style.css" />'
 
 def index(request):
-    return HttpResponse("%s<h1 style="text-align:center;">Welcome !</h1><p>" % (css), content_type='text/html')
+    return HttpResponse("%s<h1 style='text-align:center;'>Welcome !</h1><p>" % (css), content_type='text/html')
